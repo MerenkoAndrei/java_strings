f3f42ab683adc32a779269bb249f597d831b5d87
Konstantin Shibkov <shibkov.k@gmail.com>
2023-02-23 00:00:39 +0500

    Задание выполнено

--- src/main/java/practice/regex/SplitText.java
+++ src/main/java/practice/regex/SplitText.java
@@ -1,14 +1,24 @@
 package practice.regex;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class SplitText {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        String text = new Scanner(System.in).nextLine();
+        System.out.println(splitTextIntoWords(text));
 
-  }
+    }
 
-  public static String splitTextIntoWords(String text) {
-    //TODO реализуйте метод
-    return "";
-  }
-
+    public static String splitTextIntoWords(String text) {
+        StringBuilder stb = new StringBuilder();
+        Pattern pattern = Pattern.compile("\\b[A-Za-z]+\\b");
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            stb.append(matcher.group()).append("\n");
+        }
+        return stb.toString().trim();
+    }
 }
--- src/main/java/practice/string/SequentialWordsNumbers.java
+++ src/main/java/practice/string/SequentialWordsNumbers.java
@@ -1,8 +1,35 @@
 package practice.string;
 
-public class SequentialWordsNumbers {
+import java.util.Scanner;
 
-    public static String sequentialWordsNumbers(String text){
-        return "";
+public class SequentialWordsNumbers {
+    public static void main(String[] args) {
+        System.out.println("Введите текст: ");
+        String text = new Scanner(System.in).nextLine();
+        System.out.println(sequentialWordsNumbers(text));
+    }
+
+    public static String sequentialWordsNumbers(String text) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int num = 0;
+        String word = "";
+        String newText = text.trim() + " ";
+        if (text == "") {
+        } else {
+            for (int i = 0; i < newText.length(); i++) {
+                if (newText.length() <= 2 && !Character.isLetter(newText.charAt(i))) {
+                    word = "";
+                }
+                if (newText.charAt(i) != ' ' && newText.length() > 2) {
+                    word = word + newText.charAt(i);
+                } else {
+                    num++;
+                    stringBuilder.append(" (" + num + ") " + word);
+                    word = "";
+                }
+            }
+        }
+
+        return stringBuilder.toString().trim();
     }
 }
